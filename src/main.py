f259984362e11c11f1d25ecb239f4b0ffffcdffa
Eman Bautista <emanbautista410@gmail.com>
2023-01-16 01:04:28 -0500

    basic bot

--- src/main.py
+++ src/main.py
@@ -18,5 +18,30 @@ token = os.getenv('TOKEN')
 
 @client.event
 async def on_ready():
-        logger.info("GPT-3man has logged in as a bot {client.user}")
+        logger.info(f"GPT-3man has logged in as a bot {client.user}")
 
+@client.event
+async def on_message(message):
+    username = str(message.author).split("#")[0]
+    channel = str(message.channel.name)
+    user_message = str(message.content)
+
+    logger.info(f"Message {user_message} by {username} on {channel}")
+
+    if message.author == client.user:
+        return
+    
+    if channel == "random":
+        if user_message.lower() == "hello" or user_message.lower() == "hi":
+            await message.channel.send(f"Hello {username}")
+            return
+        elif user_message.lower() == "bye":
+            await message.channel.send(f'Bye {username}')
+        elif user_message.lower() == "rizz me up":
+            jokes = ["Are you a camera? Because every time I look at you, I smile.", 
+                "Do you have a map? Because I just keep getting lost in your eyes.",
+                "Excuse me, but I think you dropped something: my jaw."
+            ]
+            await message.channel.send(random.choice(jokes))
+
+client.run(token)
